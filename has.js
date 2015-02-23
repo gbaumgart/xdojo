656739a800bfd6396b23b82165ebf1c4fba4fbeb
mc007 <asd28071977>
2015-02-23 18:31:18 +0100

    =cross has added

--- has.js
+++ has.js
@@ -1,2 +1,30 @@
-define(['require'], function (require) {
-});
+(function () {
+
+    var __isAMD = !!(typeof define === 'function' && define.amd),
+        __isNode = (typeof exports === 'object'),
+        __isWeb = !__isNode,
+        __isDojoRequire = !!(typeof require === 'function' && require.packs),   //is that enough at some point?
+        __isRequireJS = !__isDojoRequire,
+        __deliteHas = !!(typeof has === 'function' && has.addModule);
+
+    define([
+            'require',
+            'exports',
+            'module',
+            __isDojoRequire ? 'dojo/has' : __deliteHas ? 'requirejs-dplugins/has' : null
+        ],
+        function (require, exports, module, dHas) {
+
+            //node.js
+            if (typeof exports !== "undefined") {
+                exports.has = has;
+            }
+            if (__isNode) {
+                return module.exports;
+            } else if (__isWeb && __isAMD) {
+                return dHas;
+            }
+
+        });
+
+}).call(this);
